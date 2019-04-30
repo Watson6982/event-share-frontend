c71c1ee9019bb0bed4814a6d1424d5c95d12353f
Watson Joshua <Joshua.Watson@cambridgeshire.gov.uk>
2019-04-30 10:41:25 +0100

    Amend sll package implementation

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
-const enforce = require('express-sslify');
 app = express();
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
+
+if(process.env.NODE_ENV === 'production') {
+    app.use((req, res, next) => {
+      if (req.header('x-forwarded-proto') !== 'https')
+        res.redirect(`https://${req.header('host')}${req.url}`)
+      else
+        next()
+    })
+}
+
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 80;
 app.listen(port);
